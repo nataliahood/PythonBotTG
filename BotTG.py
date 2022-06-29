fc1236c743f1a3234034283623a8d70a1bcfb4e6
Natalia <107213946+nataliahood@users.noreply.github.com>
2022-06-29 11:38:22 +0300

    Update BotTG.py

--- BotTG.py
+++ BotTG.py
@@ -46,11 +46,11 @@ def get_handle_text(message):
         markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
         item1 = types.KeyboardButton('Спасибо, мне уже лучше👍🏽')
         markup.add(item1)
-        bot.send_message(message.chat.id, 'Я буду присылать тебе слова поддержки каждый вечер! До тех пор, пока тебе не станет легче😌', reply_markup=markup)
+        bot.send_message(message.chat.id, 'Я буду присылать тебе слова поддержки каждые 10 минут! До тех пор, пока тебе не станет легче😌', reply_markup=markup)
         user_id = message.from_user.id
         def function_to_run():
             bot.send_message(user_id, random.choice(support))
-        schedule.every().minutes.do(function_to_run)
+        schedule.every(10).minutes.do(function_to_run)
     elif message.text.strip() == 'Спасибо, мне уже лучше👍🏽':
         stickers = str(random.choice(stic))
         markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
